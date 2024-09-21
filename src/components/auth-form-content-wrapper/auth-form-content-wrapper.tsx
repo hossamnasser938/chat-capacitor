09dff5f03e5e8c9ae1dbb9d80dd2929b1f2bb042
Hossam Abdelnaser <hossam.abdelnaser@myoncare.com>
2024-09-21 17:20:05 +0300

    fix auth image ui

--- src/components/auth-form-content-wrapper/auth-form-content-wrapper.tsx
+++ src/components/auth-form-content-wrapper/auth-form-content-wrapper.tsx
@@ -13,8 +13,8 @@ export const AuthFormContentWrapper = (props: PropsWithChildren) => {
 
   return (
     <div className="flex flex-col h-full md:flex-row">
-      <div className="hidden md:flex flex-col flex-1 justify-centenr items-center bg-blue-300">
-        <img src="/assets/img/logo.png" />
+      <div className="hidden md:flex flex-col flex-1 justify-center items-center bg-blue-300">
+        <img src="/assets/img/logo.png" className="w-1/2" />
         <h1 className="text-4xl font-bold">Welcome to Chat Capacitor</h1>
       </div>
       {renderForm()}
